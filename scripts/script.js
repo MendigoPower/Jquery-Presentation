e9861db6446935709fff0961ce5d8e19162442c2
Tatiana B <59037000+tgbeta@users.noreply.github.com>
2022-03-14 16:50:43 -0700

    Add menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
-a
-
-b
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menuBtn() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.header__inner__menu--btn')) {
+      var dropdowns = document.getElementsByClassName("header__inner__menu--btn--content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
