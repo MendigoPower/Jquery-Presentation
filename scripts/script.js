8d617bf0a86e64959fd0ac8260ccb0051f3ff907
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-03-15 16:34:23 -0700

    home page tabs update

--- scripts/script.js
+++ scripts/script.js
@@ -89,3 +89,17 @@ $(function () {
 
 //Tooltip
 $("#tooltip-ingredients").attr('title', 'If you don\'t want a very sweet recipe, you can add dark cocoa powder');
+
+
+// Tabs Buttons
+$(document).ready(function(){
+  $('div#txt_cont div:gt(0)').css('display', 'none');
+  $('#menu ul li a').click(function(event){
+    event.preventDefault();
+    var id_tab = $(this).attr('href');
+    $('#menu ul li a').removeClass('hover_tab');
+    $(this).addClass('hover_tab');
+    $('div.txt_tab:visible').hide();
+    $(id_tab).show('slide');
+  });
+});
