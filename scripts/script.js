a01245f82a076c6deed3c16758fec7fb0e0369ae
Tatiana B <59037000+tgbeta@users.noreply.github.com>
2022-03-15 16:35:29 -0700

    Add FAQ

--- scripts/script.js
+++ scripts/script.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function ($) {
   });
 });
 
-// Scroll button
+/* Scroll button */
 $(function () {
   var pagetop = $(".scroll");
   $(window).scroll(function () {
@@ -89,3 +89,32 @@ $(function () {
 
 //Tooltip
 $("#tooltip-ingredients").attr('title', 'If you don\'t want a very sweet recipe, you can add dark cocoa powder');
+
+/* JQuery Accordion Menu */
+$(document).ready(function() {
+  $(".set > a").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > a").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
+  });
+});
