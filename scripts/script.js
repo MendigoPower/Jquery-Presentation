04453e6b09b5f29f74402eb389fb46ccf2a74dbf
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-03-15 16:30:05 -0700

    home page tabs update

--- scripts/script.js
+++ scripts/script.js
@@ -88,3 +88,17 @@ $(function () {
     return false;
   });
 });
+
+
+// Tabs buttons
+$(document).ready(function(){
+  $('div#txt_cont div:gt(0)').css('display', 'none');
+  $('#menu ul li a').click(function(event){
+    event.preventDefault();
+    var id_tab = $(this).attr('href');
+    $('#menu ul li a').removeClass('hover_tab');
+    $(this).addClass('hover_tab');
+    $('div.txt_tab:visible').hide();
+    $(id_tab).show('slide');
+  });
+});
