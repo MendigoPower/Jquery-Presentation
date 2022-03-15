9d9e404c1ed2fb9f2218f8df7b926277bfe4cf40
Tatiana B <59037000+tgbeta@users.noreply.github.com>
2022-03-15 14:41:46 -0700

    Brigadeiro recipe

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/* Dropdown menu */
 function menuBtn() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.header__inner__menu--btn')) {
       var dropdowns = document.getElementsByClassName("header__inner__menu--btn--content");
