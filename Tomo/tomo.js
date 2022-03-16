d9f2731cbe83b6f7b500ab99db201a401a77a0f8
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-03-16 16:24:07 -0700

    up to date my page

--- Tomo/tomo.js
+++ Tomo/tomo.js
@@ -2,7 +2,18 @@ $(function () {
   $(".open_btn").click(function () {
     $("#overlay, .modal_window").fadeIn();
   });
+
   $(".close_btn").click(function () {
+    $("#overlay, .modal_window").fadeOut();
+  });
+
+  $(".submit_btn").click(function (event) {
+    event.preventDefault;
+    $(".mw_top").hide();
+    $(".modal_p").show();
+  });
+
+  $("#closeBtn").click(function () {
     $("#overlay, .modal_window").fadeOut();
   });
 });
