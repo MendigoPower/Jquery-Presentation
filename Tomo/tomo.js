15df40187c897c77b4e3ae43a8e1bf7662621329
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-03-17 16:40:37 -0700

    up to date my page

--- Tomo/tomo.js
+++ Tomo/tomo.js
@@ -1,16 +1,57 @@
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("Valid email address!");
+    document.form1.text1.focus();
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
+}
+
 $(function () {
-  $(".form_btn").click(function () {
-    var str = $("#email").value;
-    if (str.match(/^([a-z0-9_\.\-])+@([a-z0-9_\.\-])+[^.]$/i)) {
-      $(".overlay, .modal").fadeIn();
-    } else {
-      alert("Please enter the correct mail address again.");
-    }
+  $(".probtn").click(function () {
+    $(".overlay, .modal").fadeIn();
+  });
+
+  $(function () {
+    $(".close_btn").click(function () {
+      $(".overlay, .modal").fadeOut();
+    });
   });
 });
 
-$(function () {
-  $(".close_btn").click(function () {
-    $(".overlay, .modal").fadeOut();
-  });
-});
+// $(function () {
+//   $(".close_btn").click(function () {
+//     $(".overlay, .modal").fadeOut();
+//   });
+// });
+
+// var str = $("#email").val();
+//     if (str.match(/^([a-z0-9_\.\-])+@([a-z0-9_\.\-])+[^.]$/i)) {
+
+//       else {
+//         alert("Please enter the correct mail address again.");
+
+// $(function () {
+//   $(".form_btn").click(function () {
+//     var str = $("#email").val();
+//     if (str.match(/^([a-z0-9_\.\-])+@([a-z0-9_\.\-])+[^.]$/i)) {
+//       $(".overlay, .modal").fadeIn();
+//     } else {
+//       return true;
+//     }
+//   });
+// });
+
+// function ValidateEmail(inputText){
+//   $(".form_btn").click(function () {
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     if (inputText.value.match(mailformat)){
+//       alert("Valid email address!");
+//     } else {
+//       alert("Please enter the correct mail address again.");
+//     };
+//   }
