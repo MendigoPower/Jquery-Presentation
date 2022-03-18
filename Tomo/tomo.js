1ce38cdc56ed8e004a5a195ae1b3d6a6f3f061e1
Tomoyuki Fujii <mjr01300828@gmail.com>
2022-03-17 22:48:12 -0700

    up to date my page and fixed menu button

--- Tomo/tomo.js
+++ Tomo/tomo.js
@@ -22,36 +22,3 @@ $(function () {
     });
   });
 });
-
-// $(function () {
-//   $(".close_btn").click(function () {
-//     $(".overlay, .modal").fadeOut();
-//   });
-// });
-
-// var str = $("#email").val();
-//     if (str.match(/^([a-z0-9_\.\-])+@([a-z0-9_\.\-])+[^.]$/i)) {
-
-//       else {
-//         alert("Please enter the correct mail address again.");
-
-// $(function () {
-//   $(".form_btn").click(function () {
-//     var str = $("#email").val();
-//     if (str.match(/^([a-z0-9_\.\-])+@([a-z0-9_\.\-])+[^.]$/i)) {
-//       $(".overlay, .modal").fadeIn();
-//     } else {
-//       return true;
-//     }
-//   });
-// });
-
-// function ValidateEmail(inputText){
-//   $(".form_btn").click(function () {
-//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     if (inputText.value.match(mailformat)){
-//       alert("Valid email address!");
-//     } else {
-//       alert("Please enter the correct mail address again.");
-//     };
-//   }
